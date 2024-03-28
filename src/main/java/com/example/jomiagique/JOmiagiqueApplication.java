7847dedea5bca695828164efd31e6a86ed5ef35b
Marvinwgnr <marvin.wagner@toulouse.miage.fr>
2024-03-28 14:32:08 +0100

    Test

--- src/main/java/com/example/jomiagique/JOmiagiqueApplication.java
+++ src/main/java/com/example/jomiagique/JOmiagiqueApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JOmiagiqueApplication {
     // test
-    //MMtest
+    //MMtesttet
     public static void main(String[] args) {
         SpringApplication.run(JOmiagiqueApplication.class, args);
     }
