d07b4972cb72456898f7b81909ba0b428bb22e86
nicolas <nicolas.fontaine@toulouse.miage.fr>
2024-03-28 14:34:02 +0100

    dossier de base

--- src/main/java/com/example/jomiagique/JOmiagiqueApplication.java
+++ src/main/java/com/example/jomiagique/JOmiagiqueApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JOmiagiqueApplication {
-    // test
+    // test gg
 
     public static void main(String[] args) {
         SpringApplication.run(JOmiagiqueApplication.class, args);
