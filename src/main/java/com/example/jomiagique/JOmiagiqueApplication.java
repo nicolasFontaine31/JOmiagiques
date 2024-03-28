343c98f252e01e539b5617347eadaf5ed561e840
nicolas <nicolas.fontaine@toulouse.miage.fr>
2024-03-28 14:12:42 +0100

    test

---
+++ src/main/java/com/example/jomiagique/JOmiagiqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.jomiagique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JOmiagiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JOmiagiqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jomiagique/JOmiagiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jomiagique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JOmiagiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
